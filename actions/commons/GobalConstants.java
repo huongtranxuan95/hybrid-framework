de745ad38ad29af825963fe7542160748b76e042
tranxuanhuong <tranxuanhuong.dev@gmail.com>
2021-07-17 08:42:24 +0700

    Add file Level_12_Handle_Data_Table_Grid

--- actions/commons/GobalConstants.java
+++ actions/commons/GobalConstants.java
@@ -1,5 +1,7 @@
 package commons;
 
 public class GobalConstants {
-
+	public static final String ROOT_FOLDER = System.getProperty("user.dir");
+	//public static final DEV_URL ="http://demo.guru99.com/v4";
+	//public static final TEST_URL ="http://test.guru99.com/v4";
 }
---
+++ actions/pageObjects/livegurru/ManageCustomerPageObject.java
@@ -0,0 +1,16 @@
+package pageObjects.livegurru;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.AbstractPage;
+
+public class ManageCustomerPageObject extends AbstractPage {
+	private WebDriver driver;
+
+	public ManageCustomerPageObject(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
+
+	
+}
